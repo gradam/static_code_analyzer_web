02b0e466049e962ad220a8700a49493852c1b529
Jakub Semik <kuba.semik@gmail.com>
2017-05-06 11:49:14 +0200

    Added migration files and font awesome

--- src/analyzer/models.py
+++ src/analyzer/models.py
@@ -9,6 +9,7 @@ class Project(models.Model):
     name = models.CharField(_('Name'), max_length=100)
     url = models.CharField(_('Url'), max_length=200, unique=True)
     last_analyzed = models.DateTimeField(_('Last analyzed'), default=None, blank=True, null=True)
+    running = models.BooleanField(_('Running'), default=False)
     # List of analyzers to use for this project
     analyzers = MultiSelectField(_('analyzers'), choices=Analyzers.CHOICES, min_choices=1)
 
